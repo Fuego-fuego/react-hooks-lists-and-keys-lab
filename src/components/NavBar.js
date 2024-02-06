c363a2493d9be8f3cee807570773f2f39c502d26
nathaniEL mujesia <nathanielmujesia@gmail.com>
2024-02-06 15:03:43 +0300

    Completed assignment

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,10 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  const linksElements = links.map((link) => <a href ={`#` + link} key = {link}> {link}</a>);
+  return <nav>{/* display an <a> tag for each link here */}
+          {linksElements}  
+        </nav>;
 }
 
 export default NavBar;
